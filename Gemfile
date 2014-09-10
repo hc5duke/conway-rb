cc1ecfbbcbb3e4f19f9b9df958813d2c68781c17
Hwan-Joon Choi <hc5duke@gmail.com>
2014-09-10 00:22:50 -0700

    I'm not using these

--- Gemfile
+++
@@ -1,6 +0,0 @@
-# A sample Gemfile
-source "https://rubygems.org"
-
-group :test do
-  gem 'rspec'
-end
--- spec/conway_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rspec'
-require './conway'
-
-describe Conway do
-  it "spec_name" do
-    expect(true).to be_truthy
-  end
-end
